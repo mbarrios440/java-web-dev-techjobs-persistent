46b9fd0f88a0bae48991d52cfef43b3c2da12619
Miguel Barrios <miguelbh440@gmail.com>
2021-07-05 13:20:59 -0500

    completed part 1 and part 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
 ## Part 1: Test it with SQL
+id (int, PK), employer(varchar), name(varchar), skills(varchar)
 
 ## Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "This field can't be blank")
+    @Size(max = 120, message = "This field can't exceed 120 characters")
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
